56820389a1554c8fb3c9f9e064da90eccb0e7c5c
Scott Doxey <neogeek0101@gmail.com>
2014-07-05 17:15:42 -0400

    Added createWorld, drawDebug and step methods.

--- facadejs-Box2D.js
+++ facadejs-Box2D.js
@@ -1,16 +1,9 @@
+/*jslint browser: true, nomen: true */
+/*globals Facade, Box2D */
+
 (function (Facade) {
 
     'use strict';
-
-    function displayErrorMessage (msg) {
-
-        if (window.console !== undefined && window.console.error !== undefined) {
-
-            console.error('Facade.js Box2D Plugin -', msg);
-
-        }
-
-    }
 
     var methods = {
 
@@ -75,9 +68,11 @@
 
                 if (options.points[key] !== undefined) {
 
-                    vertices.push(new Box2D.Common.Math.b2Vec2(
-                        (options.points[key][0] / config.scale),
-                        options.points[key][1] / config.scale)
+                    vertices.push(
+                        new Box2D.Common.Math.b2Vec2(
+                            (options.points[key][0] / config.scale),
+                            options.points[key][1] / config.scale
+                        )
                     );
 
                 }
@@ -91,16 +86,16 @@
             fixture.shape = new Box2D.Collision.Shapes.b2PolygonShape();
             fixture.shape.SetAsArray(vertices, options.points.length);
 
-            world.CreateBody(body).CreateFixture(fixture);
+            world._box2d.entity.CreateBody(body).CreateFixture(fixture);
 
             if (this._box2d !== undefined) {
 
-                methods.destroyObject.call(this, world);
+                methods.destroyObject.call(this, world._box2d.entity);
 
             }
 
             this._box2d = {
-                entity: world.GetBodyList(),
+                entity: world._box2d.entity.GetBodyList(),
                 config: config,
                 callback: {
                     BeginContact: null,
@@ -111,6 +106,85 @@
             };
 
             return this;
+
+        },
+
+        createWorld: function (stage, config) {
+
+            var world,
+                listener = new Box2D.Dynamics.b2ContactListener(),
+                debugDraw = new Box2D.Dynamics.b2DebugDraw(),
+                defaults = {
+                    gravity: 40,
+                    sleep: false
+                };
+
+            if (config === undefined) {
+
+                config = {};
+
+            }
+
+            Object.keys(defaults).forEach(function (key) {
+
+                if (config[key] === undefined) {
+
+                    config[key] = defaults[key];
+
+                }
+
+            });
+
+            world = new Box2D.Dynamics.b2World(new Box2D.Common.Math.b2Vec2(0, config.gravity), config.sleep);
+
+            ['PreSolve', 'PostSolve', 'BeginContact', 'EndContact'].forEach(function (type) {
+
+                listener[type] = (function (entities) {
+
+                    var a = entities.GetFixtureA().GetBody().GetUserData(),
+                        b = entities.GetFixtureB().GetBody().GetUserData();
+
+                    if (a && typeof a._box2d.callback[this.type] === 'function') {
+
+                        a._box2d.callback[this.type].call(a, a, b);
+
+                    }
+
+                    if (b && typeof b._box2d.callback[this.type] === 'function') {
+
+                        b._box2d.callback[this.type].call(b, a, b);
+
+                    }
+
+                }).bind({ type: type });
+
+            });
+
+            world.SetContactListener(listener);
+
+            debugDraw.SetSprite(stage.context);
+            debugDraw.SetDrawScale(30);
+            debugDraw.SetFillAlpha(0.3);
+            debugDraw.SetLineThickness(1.0);
+            debugDraw.SetFlags(Box2D.Dynamics.b2DebugDraw.e_shapeBit | Box2D.Dynamics.b2DebugDraw.e_centerOfMassBit);
+            world.SetDebugDraw(debugDraw);
+
+            this._box2d = {
+                entity: world,
+                config: config
+            };
+
+            return this;
+
+        },
+
+        drawDebug: function () {
+
+            if (this._box2d.entity instanceof Box2D.Dynamics.b2World) {
+
+                this._box2d.entity.DrawDebugData();
+
+            }
 
         },
 
@@ -180,7 +254,7 @@
 
             } else {
 
-                displayErrorMessage(type + ' is not a valid callback type.');
+                console.error(type + ' is not a valid callback type.');
 
             }
 
@@ -215,21 +289,29 @@
 
             }
 
+        },
+
+        step: function () {
+
+            if (this._box2d.entity instanceof Box2D.Dynamics.b2World) {
+
+                this._box2d.entity.Step(1 / 60, 8, 3);
+
+            }
+
         }
 
     };
 
     Facade.Entity.prototype.Box2D = function (method) {
 
-        if (methods[method]) {
+        if (!methods[method]) {
 
-            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-
-        } else {
-
-            displayErrorMessage(method + ' is not a method specified in this plugin.');
+            console.error(method + ' is not a method specified in this plugin.');
 
         }
+
+        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
 
     };
 
